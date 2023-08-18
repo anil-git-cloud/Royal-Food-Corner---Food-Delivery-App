c26b6a16c2d95da67201cef68da135b303e86caf
Anil Dhangar <anildhangar2709@gmail.com>
2023-08-18 21:33:53 +0530

    Header

---
+++ src/component/Header.jsx
@@ -0,0 +1,54 @@
+import "./header.css"
+import { AiOutlineHome } from "react-icons/ai";
+import { BiHelpCircle } from "react-icons/bi";
+import { IoMdContact } from "react-icons/io";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { BsPersonAdd } from "react-icons/bs";
+import { IoIosRestaurant } from "react-icons/io";
+import logo from "./Images/logo.png"
+
+const Header = () => {
+  return (
+    <>
+        <header className="header">
+              <div className="logo">
+                  <a href="/"><img src={logo} alt="logo" /></a>
+                  <a href="/">
+                      <h3>ROYAL</h3>
+                      <p>Food Corner</p>
+                  </a>
+              </div>
+              <div className="navbar">
+                  <ul className="nav-menu">
+                      <li className="nav-menu-list">
+                          <a href="#">
+                              <span className="nav-menu-icon"><AiOutlineHome /></span>Home</a>
+                      </li>
+                      <li className="nav-menu-list">
+                          <a href="#">
+                              <span className="nav-menu-icon"><IoIosRestaurant /></span>Restaurant</a>
+                      </li>
+                      <li className="nav-menu-list">
+                          <a href="#">
+                              <span className="nav-menu-icon"><BiHelpCircle /></span>Help</a>
+                      </li>
+                      <li className="nav-menu-list">
+                          <a href="#">
+                              <span className="nav-menu-icon"><IoMdContact /></span>Contact</a>
+                      </li>
+                      <li className="nav-menu-list">
+                          <a href="#">
+                          <span className="nav-menu-icon"><BsPersonAdd/></span>Sign In</a>
+                      </li>
+                      <li className="nav-menu-list">
+                          <a href="#">
+                          <span className="nav-menu-icon"><AiOutlineShoppingCart/></span>Cart</a>
+                      </li>
+                  </ul>
+              </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/component/Navitems.jsx
@@ -0,0 +1,71 @@
+export const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    path: "./Home.jsx",
+    cName: "nav-item"
+  },
+  {
+    id: 2,
+    title: "Restaurant",
+    path: "./About.jsx",
+    cName: "nav-item"
+  },
+  {
+    id: 3,
+    title: "Contact",
+    path: "./Temple.jsx",
+    cName: "nav-item"
+  },
+  {
+    id: 4,
+    title: "Help",
+    path: "./Media.jsx",
+    cName: "nav-item"
+  },
+  {
+    id: 5,
+    title: "Sign In",
+    path: "./Donate.jsx",
+    cName: "nav-item"
+  },
+  {
+    id: 6,
+    title: "Cart",
+    path: "./Contact.jsx",
+    cName: "nav-item"
+  }
+];
+
+export const aboutsubMenu = [
+  {
+    id: 1,
+    title: "About ISKCON",
+    path: "./Aboutiskcon",
+    cName: "menu-item"
+  },
+  {
+    id: 2,
+    title: "ISKCON THANE",
+    path: "./iskconthane",
+    cName: "menu-item"
+  },
+  {
+    id: 3,
+    title: "our Mission",
+    path: "./Ourmission",
+    cName: "menu-item"
+  },
+  {
+    id: 4,
+    title: "Founder Acharya",
+    path: "./founderacharya",
+    cName: "menu-item"
+  },
+  {
+    id: 5,
+    title: "Our Beliefs",
+    path: "./ourbeliefs",
+    cName: "menu-item"
+  }
+];
